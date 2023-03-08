1c15a2c7d41e7c7e8e64e74dc11fdeec50ecfc2a
Pasupuletilakshmipriyanka <110388192+Pasupuletilakshmipriyanka@users.noreply.github.com>
2023-03-08 20:00:51 +0530

    Add files via upload

---
+++ double linked list creation.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.previous=None
+        self.next=None
+class doublelinkedlist:
+    def __init__(self):
+        self.head=None   
+    def display(self):
+        if self.head is None:
+            print("Linked List is empty")
+        else:
+            temp=self.head
+            while temp:
+                print(temp.data,"<-->",end=" ")
+                temp=temp.next
+obj=doublelinkedlist()
+n1=Node(100)
+obj.head=n1
+n2=Node(200)
+n2.prev=n1
+n1.next=n2
+obj.display()
+
+            
---
+++ sll delete first node.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class singlelinkedlist:
+    def __init__(self):
+        self.head=None
+    def delete(self):
+        temp=self.head
+        self.head=temp.next
+        temp.next=None    
+    def display(self):
+        if self.head is None:
+            print("Linked List is empty")
+        else:
+            temp=self.head
+            while temp:
+                print(temp.data,"-->",end=" ")
+                temp=temp.next
+obj=singlelinkedlist()
+n=Node(10)
+obj.head=n
+n1=Node(20)
+n.next=n1
+n2=Node(30)
+n1.next=n2
+n3=Node(40)
+n2.next=n3
+n4=Node(50)
+n3.next=n4
+obj.display()
+print("\n")
+obj.delete()
+obj.display()
+            
